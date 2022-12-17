b9f8282924857b7e6e8cd566fcd621718c5b28b6
KAMIL.NAJA <KAMIL.NAJA@EXTERNAL.NEXIGROUP.COM>
2022-12-17 19:20:02 +0100

    initial commit

---
+++ index.ts
@@ -0,0 +1,14 @@
+import dotenv from "dotenv";
+import express, { Request, Response } from "express";
+dotenv.config();
+
+const app = express();
+const port = process.env.PORT;
+
+app.get("/", (req: Request, res: Response) => {
+  res.send("Express + TypeScript Server");
+});
+
+app.listen(port, () => {
+  console.log(`[server]: 🌩️Server is running at https://localhost:${port}`);
+});
