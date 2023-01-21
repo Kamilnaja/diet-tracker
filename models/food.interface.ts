74465ca478325c9948fb3462fffedf33be800d79
KAMIL.NAJA <KAMIL.NAJA@EXTERNAL.NEXIGROUP.COM>
2023-01-21 17:57:50 +0100

    removing unused interface

--- models/food.interface.ts
+++ models/food.interface.ts
@@ -1,11 +1,8 @@
 import { NutriScore } from "./nutri-score.enum";
 import { IResponse } from "./response.interface";
 
-export interface IFoodEntity extends IFood {
+export interface IFoodEntity {
   id: number;
-}
-
-export interface IFood {
   name: string;
   weight: number;
   caloriesPer100g: number;
