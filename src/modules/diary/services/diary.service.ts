2e66ceaa99d25f06ffc25a948282a9d5255cdf7f
KAMIL.NAJA <KAMIL.NAJA@EXTERNAL.NEXIGROUP.COM>
2023-04-16 13:28:24 +0200

    deleting diary items

--- src/modules/diary/services/diary.service.ts
+++ src/modules/diary/services/diary.service.ts
@@ -34,6 +34,29 @@ export class DiaryService {
     return reduced;
   };
 
+  static getDiaryEntryById = async (id: number): Promise<Diary> => {
+    const query = `    
+    select df.diary_id, d.date, fid.id as food_id, fid.weight, fid.meal_type, fid.date_added 
+    FROM diary_foods df
+    INNER JOIN food_in_diary fid
+    ON df.food_id = fid.id
+    INNER JOIN diary d 
+    ON df.diary_id = d.id
+    WHERE d.id LIKE '%' || ? || '%'
+    `;
+
+    let rows = db.all(query, [id]);
+    return rows;
+  };
+
+  static deleteDiaryItemById = async (id: string): Promise<Diary> => {
+    const query = `
+    DELETE FROM diary_foods WHERE diary_id = ?`;
+
+    let rows = await db.run(query, [id]);
+    return rows;
+  };
+
   private static groupDiaryById(rows: any): Diary[] {
     return rows.reduce((acc: any, item: any) => {
       let { diary_id } = item;
