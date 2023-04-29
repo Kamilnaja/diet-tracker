6bbf6ddc3fb4c06fb1f0c99132a176799d26686f
KAMIL.NAJA <KAMIL.NAJA@EXTERNAL.NEXIGROUP.COM>
2023-04-29 11:18:21 +0200

    better deleting script

--- src/db/db-drop-scripts.ts
+++ src/db/db-drop-scripts.ts
@@ -2,15 +2,12 @@ import { db } from "./db";
 import { tables } from "./db-table-names";
 
 export async function dropTables(): Promise<void> {
-  Promise.all([
-    await db.run(`DROP TABLE IF EXISTS ${tables.FOODS};`),
-    await db.run(`DROP TABLE IF EXISTS ${tables.TAGS};`),
-    await db.run(`DROP TABLE IF EXISTS ${tables.FOOD_TAGS};`),
-    await db.run(`DROP TABLE IF EXISTS ${tables.DIARY};`),
-    await db.run(`DROP TABLE IF EXISTS ${tables.DIARY_FOODS};`),
-    await db.run(`DROP TABLE IF EXISTS ${tables.FOOD_IN_DIARY};`),
-    await db.run(`DROP TABLE IF EXISTS ${tables.USERS};`),
-  ])
-    .then(() => console.log("All tables dropped successfully"))
-    .catch((err: Error) => console.error(err));
+  for await (const item of Object.values(tables)) {
+    await db
+      .run(`DROP TABLE IF EXISTS ${item};`)
+      .then(() => {
+        console.log(`🧨 ${item} table has been dropped 🧨`);
+      })
+      .catch((err: Error) => console.error(err));
+  }
 }
--- src/db/db-table-names.ts
+++ src/db/db-table-names.ts
@@ -2,13 +2,11 @@ const FOODS = "foods";
 const TAGS = "tags";
 const FOOD_TAGS = "food_tags";
 const DIARY = "diary";
-/*
-  This table is used to store the relationship between diary entries and food items.
-*/
+
+// This table is used to store the relationship between diary entries and food items.
 const DIARY_FOODS = "diary_foods";
-/*
-  this table is used for store foods in diary
-*/
+
+// this table is used for store foods in diary
 const FOOD_IN_DIARY = "food_in_diary";
 const USERS = "users";
 
