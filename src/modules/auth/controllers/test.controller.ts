b8b29eb2fac91d85763def3eb0aeee7d28dfc6e9
kamilnaja <kamilnaja@gmail.com>
2024-04-28 15:57:54 +0200

    Remove unused test controller and routes

--- src/modules/auth/controllers/test.controller.ts
+++
@@ -1,8 +0,0 @@
-import { Request, Response } from "express";
-
-export const userBoard = async (req: Request, res: Response): Promise<void> => {
-  /*
-  #swagger.tags = ['Test']
-  */
-  res.status(200).json({ message: "User Content." });
-};
